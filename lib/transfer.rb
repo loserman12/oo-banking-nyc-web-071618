d78311576f864e34a900b4a763a0da10af45b2cb
Your Name <you@example.com>
2018-07-19 16:02:20 -0400

    save

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
+require_relative './bank_account.rb'
+
 class Transfer
-  # your code here
+
+ attr_reader :sender, :receiver, :amount
+ attr_accessor :status
+
+ def initialize(sender, receiver, amount, status="pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? && sender.balance > amount
+  end
+
+  def execute_transaction
+    if @status == pending && self.valid? == true
+      @sender.balance
+  end
+
+  def re
 end
